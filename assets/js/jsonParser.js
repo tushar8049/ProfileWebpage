0ef34dbbccfa176f7f8eb970636d18fbd2e38494
tushar-gonawala <tushar.gonawala@copart.com>
2019-06-29 06:58:46 -0500

    index.html

---
+++ assets/js/jsonParser.js
@@ -0,0 +1,15 @@
+function loadProjects(){
+    var project = [];
+
+    $.getJSON('project.json', function(data) {
+        console.log(data)
+        $.each(data.project, function(i, f) {
+            console.log(f.firstName);
+            // var tblRow = "<tr>" + "<td>" + f.firstName + "</td>" +
+            //     "<td>" + f.lastName + "</td>" + "<td>" + f.job + "</td>" + "<td>" + f.roll + "</td>" + "</tr>"
+            // $(tblRow).appendTo("#userdata tbody");
+        });
+
+    });
+
+}
